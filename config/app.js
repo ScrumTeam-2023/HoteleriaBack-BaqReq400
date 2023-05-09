143a28cedcd6a445cb8fbbe6dfcee6230f12aad4
M <malvarez-2018477@kinal.edu.gt>
2023-05-08 22:24:51 -0600

    -se Agrego ruta "User" en app.js

--- config/app.js
+++ config/app.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 3000;
 
 //const NombreRoutes = require('Ruta')
 const eventRoutes = require ('../src/events/event.routes');
+const userRoutes = require('../src/user/user.routes')
 
 //server Config
 app.use(express.urlencoded({extended: false}));
@@ -19,6 +20,7 @@ app.use(morgan('dev'))
 
 //Agregar Rutas de Cada Entidad
 app.use('/event',eventRoutes);
+app.use('/user',userRoutes);
 
 //Server
 exports.initServer = ()=>{
