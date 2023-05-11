a81223e5615c133570fde099a17d95f943db860b
ctomas2021215 <ctomas-2021215@kinal.edu.gt>
2023-05-11 13:35:41 -0600

    Se agrego servicios Adicionales

--- config/app.js
+++ config/app.js
@@ -7,6 +7,8 @@ const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3000;
 //server Config
+const servicesRoutes = require('../src/roomServices/rs.routes')
+
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json())
@@ -15,6 +17,8 @@ app.use(helmet())
 app.use(morgan('dev'))
 
 //Agregar Rutas de Cada Entidad
+app.use('/services',servicesRoutes);
+
 //const NombreRoutes = require('Ruta')
 
 //Server
