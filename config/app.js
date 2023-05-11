28201d8dc59a0229a53f2d24916e2fc3529a8664
malvarez-2018477 <malvarez-2018477@kinal.edu.gt>
2023-05-11 14:39:28 -0600

    -se agrego Servicios (RoomServices)
    -se Agrego cambios menores
    -se Arreglo bug con Administrador

--- config/app.js
+++ config/app.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 3000;
 //const NombreRoutes = require('Ruta')
 const eventRoutes = require ('../src/events/event.routes');
 const userRoutes = require('../src/user/user.routes')
+const servicesRoutes = require('../src/roomServices/rs.routes')
 
 //server Config
 app.use(express.urlencoded({extended: false}));
@@ -21,7 +22,7 @@ app.use(morgan('dev'))
 //Agregar Rutas de Cada Entidad
 app.use('/event',eventRoutes);
 app.use('/user',userRoutes);
-
+app.use('/services',servicesRoutes);
 //Server
 exports.initServer = ()=>{
     app.listen(port);
